ad69936cd3e4c4e0f6e6b602085a1e06b26eb69a
Adp-797 <104199235+Adp-797@users.noreply.github.com>
2022-05-06 16:00:01 +0530

    Update Zigbee_Send.py

--- Zigbee_Send.py
+++ Zigbee_Send.py
@@ -4,7 +4,7 @@ from serial import Serial
 ser = serial.Serial('/dev/ttyUSB0', 9600,timeout=.5)
  
 while True:
-    ser.write('Hello User \r\n')         # write a Data
+    ser.write('Hello User \r\n'.encode('utf-8'))         # write a Data
     print("Data Sent")
     #incoming = ser.readline().strip()
     #print 'Received Data : '+ incoming
