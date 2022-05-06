324663c00e6583aadddd3c4042a98ce3ee17be75
Adp-797 <104199235+Adp-797@users.noreply.github.com>
2022-05-06 16:03:42 +0530

    Update Zigbee_Send.py

--- Zigbee_Send.py
+++ Zigbee_Send.py
@@ -1,7 +1,7 @@
 from serial import Serial
  
 # Enable USB Communication
-ser = serial.Serial('/dev/ttyUSB0', 9600,timeout=.5)
+ser = Serial('/dev/ttyUSB0', 9600,timeout=.5)
  
 while True:
     ser.write('Hello User \r\n'.encode('utf-8'))         # write a Data
